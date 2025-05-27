335915f86c30e46586de3095e2e23123590b14c6
Michael Utz <michael@theutz.com>
2025-05-27 07:22:14 +0300

    style(kocaeli): refactor config for readability and increase gap size
    - Restructure configuration using flat dot notation for better
    readability
    - Increase window gaps from 8 to 16 pixels in Aerospace settings
    - Add lib to function parameters
    - Improve formatting with consistent spacing
    
    Signed-off-by: Michael Utz <michael@theutz.com>

--- systems/aarch64-darwin/kocaeli/default.nix
+++ systems/aarch64-darwin/kocaeli/default.nix
@@ -1,4 +1,5 @@
-{ pkgs, namespace, inputs, system, ...  }: {
+{ pkgs, namespace, inputs, system, lib, ...  }: {
+
         environment.shellAliases = {
                 gcam = "git commit --all --message";
                 gcm = "git commit --message";
@@ -8,12 +9,14 @@
                 gws = "git status --short";
                 gwS = "git status";
         };
+
         environment.shells = with pkgs; [
                 bashInteractive
                 fish
                 zsh
                 nushell
         ];
+
         environment.systemPackages = (with pkgs; [
                 ripgrep
                 pam-reattach
@@ -21,115 +24,83 @@
         ]) ++ (with pkgs.${namespace}; [
                 nvim
         ]);
-        environment.etc."pam.d/sudo_local" = {
-                text = ''
-                        auth    optional        ${pkgs.pam-reattach}/lib/pam/pam_reattach.so
-                        auth    sufficient      pam_tid.so
-                '';
-        };
-        homebrew = {
-                enable = true;
-                brews = [];
-                casks = [
-                        "vivaldi"
-                        "karabiner-elements"
-                ];
-                onActivation = {
-                        autoUpdate = false; # default
-                        cleanup = "zap";
-                };
-                taps = [];
-        };
-	home-manager = {
-		backupFileExtension = "bak";
-		useUserPackages = true;
-		useGlobalPkgs = true;
-	};
-	nix = {
-                enable = true;
-		checkConfig = true;
-		nixPath = {
-			nixpkgs = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
-		};
-	};
+
+        environment.etc."pam.d/sudo_local".text = ''
+                auth    optional        ${pkgs.pam-reattach}/lib/pam/pam_reattach.so
+                auth    sufficient      pam_tid.so
+        '';
+
+        homebrew.enable = true;
+        homebrew.brews = [];
+        homebrew.casks = [
+                "vivaldi"
+                "karabiner-elements"
+        ];
+        homebrew.onActivation.autoUpdate = false; # default
+        homebrew.onActivation.cleanup = "zap";
+        homebrew.taps = [];
+
+        home-manager.backupFileExtension = "bak";
+        home-manager.useUserPackages = true;
+        home-manager.useGlobalPkgs = true;
+
+        nix.enable = true;
+        nix.checkConfig = true;
+        nix.nixPath.nixpkgs = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+
 	programs.fish.enable = true;
+
         programs.man.enable = true;
+
         programs.nix-index.enable = true;
-        programs.tmux = {
-                enable = true;
-                enableSensible = true;
-                enableFzf = true;
-                enableMouse = true;
-                enableVim = true;
+
+        programs.tmux.enable = true;
+        programs.tmux.enableSensible = true;
+        programs.tmux.enableFzf = true;
+        programs.tmux.enableMouse = true;
+        programs.tmux.enableVim = true;
+
+        programs.vim.enable = true;
+        programs.vim.enableSensible = true;
+
+        programs.zsh.enable = true;
+        programs.zsh.enableBashCompletion = true;
+        programs.zsh.enableCompletion = true;
+        programs.zsh.enableFastSyntaxHighlighting = true;
+        programs.zsh.enableFzfCompletion = true;
+        programs.zsh.enableFzfGit = true;
+        programs.zsh.enableFzfHistory = true;
+
+        security.pam.services.sudo_local.touchIdAuth = true;
+
+	services.aerospace.enable = true;
+        services.aerospace.settings.gaps.outer = lib.genAttrs ["left" "right" "top" "bottom"] (x:  16);
+
+        # FIXME: When https://github.com/nix-darwin/nix-darwin/issues/1041 is fixed, we can use
+        # Karabiner Elements services through nix. Until then, c'est la vie.
+        # services.karabiner-elements = {
+        #         enable = true;
+        #         package = pkgs.karabiner-elements.overrideAttrs (old: {
+        #               version = "15.3.0";
+        #
+        #               src = pkgs.fetchurl {
+        #                       inherit (old.src) url;
+        #                       hash = "sha256-Szf2mBC8c4JA3Ky4QPTvS4GJ0PXFbN0Y7Rpum9lRABE=";
+        #               };
+        #
+        #               dontFixup = true;
+        #         });
+        # };
+
+	system.checks.verifyBuildUsers = true;
+        system.checks.verifyNixPath = false; # not useful with flakes
+        system.primaryUser = "michael";
+        system.stateVersion = 5;
+
+	users.users.michael = {
+                description = "Michael Utz";
+                home = "/Users/michael";
+                shell = pkgs.zsh;
+                uid = 501;
         };
-        programs.vim = {
-                enable = true;
-                enableSensible = true;
-        };
-        programs.zsh = {
-                enable = true;
-                enableBashCompletion = true;
-                enableCompletion = true;
-                enableFastSyntaxHighlighting = true;
-                enableFzfCompletion = true;
-                enableFzfGit = true;
-                enableFzfHistory = true;
-	};
-        security = {
-                pam = {
-                        services = {
-                                sudo_local = {
-                                        touchIdAuth = true;
-                                };
-                        };
-                };
-        };
-	services = {
-                aerospace = {
-                        enable = true;
-                        settings = {
-                                gaps = let
-                                        gap = 8;
-                                in {
-                                        outer = {
-                                          left = gap;
-                                          right = gap;
-                                          top = gap;
-                                          bottom = gap;
-                                        };
-                                };
-                        };
-                };
-                # FIXME: When https://github.com/nix-darwin/nix-darwin/issues/1041 is fixed, we can use
-                # Karabiner Elements services through nix. Until then, c'est la vie.
-                # karabiner-elements = {
-                #         enable = true;
-                #         package = pkgs.karabiner-elements.overrideAttrs (old: {
-                #               version = "15.3.0";
-                #
-                #               src = pkgs.fetchurl {
-                #                       inherit (old.src) url;
-                #                       hash = "sha256-Szf2mBC8c4JA3Ky4QPTvS4GJ0PXFbN0Y7Rpum9lRABE=";
-                #               };
-                #
-                #               dontFixup = true;
-                #         });
-                # };
-	};
-	system = {
-		checks = {
-			verifyBuildUsers = true;
-			verifyNixPath = false; # not useful with flakes
-		};
-                primaryUser = "michael";
-		stateVersion = 5;
-	};
-	users = {
-		users.michael = {
-			description = "Michael Utz";
-			home = "/Users/michael";
-                        shell = pkgs.zsh;
-			uid = 501;
-		};
-	};
 }
