8db07b87e6ff3efb484f9fd17545fb4d4ef16166
Michael Utz <michael@theutz.com>
2025-05-28 14:12:58 +0300

    feat: setup devshell
    Signed-off-by: Michael Utz <michael@theutz.com>

--- parts/default.nix
+++ parts/default.nix
@@ -3,5 +3,6 @@
     ./pkgs # Per-system packages exposed by flake
 
     ./fmt.nix # Formatter configurations for tree-fmt
+    ./shell.nix # `nix develop` shell for working on this flake
   ];
 }
---
+++ parts/shell.nix
@@ -0,0 +1,40 @@
+{
+  perSystem = {
+    inputs',
+    config,
+    pkgs,
+    system,
+    ...
+  }: {
+    devShells.default = pkgs.mkShellNoCC {
+      name = "slashfiles";
+      meta.description = ''
+        The default development shell for my Nix flake.
+      '';
+
+      inputsFrom = [config.treefmt.build.devShell];
+
+      NH_DARWIN_FLAKE = "/etc/nix-darwin";
+
+      packages =
+        [
+          # From flake-parts modules
+          config.treefmt.build.wrapper # Use `treefmt` command
+        ]
+        ++ (with pkgs; [
+          git
+          lazygit
+          nh
+          onefetch
+        ]);
+
+      shellHook =
+        /*
+        bash
+        */
+        ''
+          onefetch
+        '';
+    };
+  };
+}
