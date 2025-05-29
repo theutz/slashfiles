aa9e31ba0a93aca817a27646cb4576570ec21653
Michael Utz <michael@theutz.com>
2025-05-29 23:15:09 +0300

    feat(nvf): add buffer management keymaps and bufdelete plugin

--- packages/nvf/keymaps.nix
+++ packages/nvf/keymaps.nix
@@ -2,27 +2,39 @@
   inherit (lib.nvim.binds) mkKeymap;
 in {
   config.vim = {
+    startPlugins = ["bufdelete-nvim"];
+
     binds.whichKey.enable = true;
 
     binds.whichKey.register = {
       "<leader>g" = "git";
       "<leader>s" = "search";
       "<leader>u" = "ui/toggle";
+      "<leader>b" = "buffers";
     };
 
     keymaps = [
+      (mkKeymap ["n"] "<leader>bd"
+        "<cmd>Bdelete<cr>" {desc = "Delete buffer";})
+
       (mkKeymap ["n"] "<leader>,"
         "<cmd>FzfLua buffers<cr>" {desc = "Open buffers...";})
+
       (mkKeymap ["n"] "<leader>/"
         "<cmd>FzfLua grep<cr>" {desc = "Search project...";})
+
       (mkKeymap ["n"] "<leader>gg"
         "<cmd>Neogit<cr>" {desc = "Neogit";})
+
       (mkKeymap ["n"] "<leader>qq"
         "<cmd>xa<cr>" {desc = "Save all and quit";})
+
       (mkKeymap ["n"] "<leader>e"
         "<cmd>Yazi<cr>" {desc = "Open file explorer...";})
+
       (mkKeymap ["n"] "<leader> "
         "<cmd>FzfLua files<cr>" {desc = "Open files...";})
+
       (mkKeymap ["n"] "<leader>sh"
         "<cmd>FzfLua helptags<cr>" {desc = "Search help";})
 
