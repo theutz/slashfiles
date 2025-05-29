f18403417143bd4cd63e652b655bdf1f9dfca9eb
Michael Utz <michael@theutz.com>
2025-05-29 16:25:01 +0300

    feat(darwin/brew): add enable option to control activation

--- modules/darwin/brew/default.nix
+++ modules/darwin/brew/default.nix
@@ -1,5 +1,16 @@
-{pkgs, ...}: {
-  config = {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  mod = "brew";
+  cfg = config.${mod};
+in {
+  options.${mod} = {
+    enable = lib.mkEnableOption "defaults for managing homebrew";
+  };
+  config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       onefetch
     ];
