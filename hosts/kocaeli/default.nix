9d23a996ea41c6b9313b24475b4ebd21799eeacc
Michael Utz <michael@theutz.com>
2025-05-29 16:25:32 +0300

    feat: enable brew for kocaeli host

--- hosts/kocaeli/default.nix
+++ hosts/kocaeli/default.nix
@@ -5,6 +5,7 @@
   lib,
   ...
 }: {
+  brew.enable = true;
   environment.etc."pam.d/sudo_local".text = ''
     auth    optional        ${pkgs.pam-reattach}/lib/pam/pam_reattach.so
     auth    sufficient      pam_tid.so
