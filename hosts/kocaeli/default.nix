27b00837823f736a7fc170ff8103ab426c847375
Michael Utz <michael@theutz.com>
2025-05-29 16:38:52 +0300

    fix: disable Homebrew on kocaeli host

--- hosts/kocaeli/default.nix
+++ hosts/kocaeli/default.nix
@@ -5,7 +5,7 @@
   lib,
   ...
 }: {
-  brew.enable = true;
+  brew.enable = false;
   environment.etc."pam.d/sudo_local".text = ''
     auth    optional        ${pkgs.pam-reattach}/lib/pam/pam_reattach.so
     auth    sufficient      pam_tid.so
