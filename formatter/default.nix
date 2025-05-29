d567c3ca31db49a17e0c31dce83f6778586fafd8
Michael Utz <michael@theutz.com>
2025-05-29 19:52:04 +0300

    chore: update docker dev environment with new package.json dependencies

--- formatter/default.nix
+++ formatter/default.nix
@@ -5,13 +5,18 @@
     treefmt = {
       projectRootFile = "flake.nix";
       enableDefaultExcludes = true;
+      settings = {
+        global.excludes = ["*.age" "*.sops*" "*.envrc"];
+      };
 
       programs = {
         alejandra.enable = true;
+        taplo.enable = true;
         shellcheck.enable = true;
-
-        shfmt.enable = true;
-        shfmt.indent_size = 2;
+        shfmt = {
+          enable = true;
+          indent_size = 2;
+        };
       };
     };
   };
