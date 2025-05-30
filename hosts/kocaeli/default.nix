5a68f2a647ccb211506d2f8700bcdb0d476eb0ad
Michael Utz <michael@theutz.com>
2025-05-30 16:56:19 +0300

    refactor(kocaeli): group configuration attributes into nested attribute sets refactor(hosts/kocaeli): organize configuration into attribute sets

--- hosts/kocaeli/default.nix
+++ hosts/kocaeli/default.nix
@@ -7,129 +7,151 @@
 }: {
   brew.enable = false;
 
-  environment.etc."pam.d/sudo_local".text = ''
-    auth    optional        ${pkgs.pam-reattach}/lib/pam/pam_reattach.so
-    auth    sufficient      pam_tid.so
-  '';
+  environment = {
+    etc."pam.d/sudo_local".text = ''
+      auth    optional        ${pkgs.pam-reattach}/lib/pam/pam_reattach.so
+      auth    sufficient      pam_tid.so
+    '';
 
-  environment.pathsToLink = [
-    "/opt/homebrew/bin"
-    "/opt/homebrew/sbin"
-  ];
+    pathsToLink = [
+      "/opt/homebrew/bin"
+      "/opt/homebrew/sbin"
+    ];
 
-  environment.shellAliases = {
-    gcam = "git commit --all --message";
-    gcm = "git commit --message";
-    gf = "git fetch";
-    gfm = "git pull";
-    gpp = "git pull && git push";
-    gws = "git status --short";
-    gwS = "git status";
-    la = "ls -la";
-    ll = "ls -l";
-    nd = "man 5 configuration.nix";
-    nvf = "man 5 nvf";
-    hm = "man 5 home-configuration.nix";
+    shellAliases = {
+      gcam = "git commit --all --message";
+      gcm = "git commit --message";
+      gf = "git fetch";
+      gfm = "git pull";
+      gpp = "git pull && git push";
+      gws = "git status --short";
+      gwS = "git status";
+      la = "ls -la";
+      ll = "ls -l";
+      nd = "man 5 configuration.nix";
+      nvf = "man 5 nvf";
+      hm = "man 5 home-configuration.nix";
+    };
+
+    shells = with pkgs; [
+      bashInteractive
+      fish
+      zsh
+      nushell
+    ];
+
+    systemPackages = [
+      # Packages from my flake inputs
+      inputs'.nh.packages.default
+
+      # Setup Neovim Flake
+      inputs'.nvf.packages.docs-manpages # generate manpages
+      packages.nvf
+
+      # Normal nixpkgs packages
+      pkgs.ripgrep
+      pkgs.pam-reattach
+      pkgs.fd
+      pkgs.git
+    ];
+
+    variables = {
+      EDITOR = lib.mkForce (lib.getExe packages.nvf);
+      NH_FLAKE = "/etc/nix-darwin";
+    };
   };
 
-  environment.shells = with pkgs; [
-    bashInteractive
-    fish
-    zsh
-    nushell
-  ];
-
-  environment.systemPackages = [
-    # Packages from my flake inputs
-    inputs'.nh.packages.default
-
-    # Setup Neovim Flake
-    inputs'.nvf.packages.docs-manpages # generate manpages
-    packages.nvf
-
-    # Normal nixpkgs packages
-    pkgs.ripgrep
-    pkgs.pam-reattach
-    pkgs.fd
-    pkgs.git
-  ];
-
-  environment.variables = {
-    EDITOR = lib.mkForce (lib.getExe packages.nvf);
-    NH_FLAKE = "/etc/nix-darwin";
+  homebrew = {
+    enable = true;
+    brews = [
+      "dark-mode"
+    ];
+    casks = [
+      "vivaldi"
+      "karabiner-elements"
+      "spotify"
+      "slack"
+      "telegram"
+      "mouseless@preview"
+    ];
+    onActivation = {
+      autoUpdate = false; # default
+      cleanup = "zap";
+    };
+    taps = [];
   };
 
-  homebrew.enable = true;
-  homebrew.brews = [];
-  homebrew.casks = [
-    "vivaldi"
-    "karabiner-elements"
-    "spotify"
-    "slack"
-    "telegram"
-    "mouseless@preview"
-    "dark-mode"
-  ];
-  homebrew.onActivation.autoUpdate = false; # default
-  homebrew.onActivation.cleanup = "zap";
-  homebrew.taps = [];
-
-  home-manager.backupFileExtension = "bak";
-  home-manager.useUserPackages = true;
-  home-manager.useGlobalPkgs = true;
-  home-manager.users.michael = ../../homes/michael;
-  home-manager.extraSpecialArgs = {inherit packages;};
-
-  nix.enable = true;
-  nix.checkConfig = true;
-  # nix.nixPath.nixpkgs = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-  nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
+  home-manager = {
+    backupFileExtension = "bak";
+    useUserPackages = true;
+    useGlobalPkgs = true;
+    users.michael = ../../homes/michael;
+    extraSpecialArgs = {inherit packages;};
   };
+
+  nix = {
+    enable = true;
+    checkConfig = true;
+    # nix.nixPath.nixpkgs = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+    };
+  };
+
   nixpkgs.config.allowUnfree = true;
 
-  programs.fish.enable = true;
+  programs = {
+    fish.enable = true;
 
-  programs.man.enable = true;
+    man.enable = true;
 
-  programs.nix-index.enable = true;
+    nix-index.enable = true;
 
-  programs.vim.enable = true;
-  programs.vim.enableSensible = true;
+    vim.enable = true;
+    vim.enableSensible = true;
 
-  programs.zsh.enable = true;
-  programs.zsh.enableBashCompletion = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.enableFastSyntaxHighlighting = true;
-  programs.zsh.enableFzfCompletion = true;
-  programs.zsh.enableFzfGit = true;
-  programs.zsh.enableFzfHistory = true;
+    zsh = {
+      enable = true;
+      enableBashCompletion = true;
+      enableCompletion = true;
+      enableFastSyntaxHighlighting = true;
+      enableFzfCompletion = true;
+      enableFzfGit = true;
+      enableFzfHistory = true;
+    };
+  };
 
   security.pam.services.sudo_local.touchIdAuth = true;
 
-  services.aerospace.enable = true;
-  services.aerospace.settings.gaps.outer = pkgs.lib.genAttrs ["left" "right" "top" "bottom"] (_x: 16);
+  services = {
+    # FIXME: When https://github.com/nix-darwin/nix-darwin/issues/1041 is fixed, we can use
+    # Karabiner Elements services through nix. Until then, c'est la vie.
+    # services.karabiner-elements = {
+    #         enable = true;
+    #         package = pkgs.karabiner-elements.overrideAttrs (old: {
+    #               version = "15.3.0";
+    #
+    #               src = pkgs.fetchurl {
+    #                       inherit (old.src) url;
+    #                       hash = "sha256-Szf2mBC8c4JA3Ky4QPTvS4GJ0PXFbN0Y7Rpum9lRABE=";
+    #               };
+    #
+    #               dontFixup = true;
+    #         });
+    # };
 
-  # FIXME: When https://github.com/nix-darwin/nix-darwin/issues/1041 is fixed, we can use
-  # Karabiner Elements services through nix. Until then, c'est la vie.
-  # services.karabiner-elements = {
-  #         enable = true;
-  #         package = pkgs.karabiner-elements.overrideAttrs (old: {
-  #               version = "15.3.0";
-  #
-  #               src = pkgs.fetchurl {
-  #                       inherit (old.src) url;
-  #                       hash = "sha256-Szf2mBC8c4JA3Ky4QPTvS4GJ0PXFbN0Y7Rpum9lRABE=";
-  #               };
-  #
-  #               dontFixup = true;
-  #         });
-  # };
+    aerospace = {
+      enable = true;
+      settings.gaps.outer = pkgs.lib.genAttrs ["left" "right" "top" "bottom"] (_x: 16);
+    };
+  };
 
-  system.checks.verifyBuildUsers = true;
-  system.checks.verifyNixPath = false; # not useful with flakes
-  system.primaryUser = "michael";
-  system.stateVersion = 5;
+  system = {
+    checks.verifyBuildUsers = true;
+    checks.verifyNixPath = false; # not useful with flakes
+    primaryUser = "michael";
+    stateVersion = 5;
+  };
 
   users.users.michael = {
     description = "Michael Utz";
