564f341e9361f77982dab591655921d9a2d7624d
Michael Utz <michael@theutz.com>
2025-05-30 17:40:13 +0300

    fix(darwin): dynamically import all modules except current file feat: update Darwin module to import all modules in directory

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,5 +1,9 @@
-{...}: {
-  imports = [
-    ./brew
+{lib, ...}: let
+  currentFile = /. + __curPos.file;
+  modules = lib.pipe ./. [
+    lib.filesystem.listFilesRecursive
+    (lib.filter (f: f != currentFile))
   ];
+in {
+  imports = modules;
 }
