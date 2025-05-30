130bb326d030ca483c485cdee7d00e5c438602bf
Michael Utz <michael@theutz.com>
2025-05-30 13:42:38 +0300

    feat(nvf): extract miscellaneous configuration to separate file feat(nvf): extract misc configuration to a separate file
    The commit refactors the Neovim configuration by moving misc settings from package.nix to a new misc.nix file, and simplifies the module loading process by using the modules parameter directly.

---
+++ packages/nvf/misc.nix
@@ -0,0 +1,51 @@
+{pkgs, ...}: {
+  config = {
+    fzf-lua.enable = true;
+    session.enable = true;
+
+    vim = {
+      formatter.conform-nvim.enable = true;
+
+      git.enable = true;
+
+      languages = {
+        enableExtraDiagnostics = true;
+        enableFormat = true;
+        enableTreesitter = true;
+        nix.enable = true;
+        nix.extraDiagnostics = {
+          enable = true;
+          types = ["statix" "deadnix"];
+        };
+        bash.enable = true;
+      };
+
+      lazy.plugins = {
+        neogit = {
+          package = pkgs.vimPlugins.neogit;
+        };
+      };
+
+      statusline.lualine.enable = true;
+
+      theme = {
+        enable = true;
+        name = "catppuccin";
+        style = "mocha";
+        transparent = true;
+      };
+
+      ui.borders.enable = true;
+
+      utility = {
+        motion = {
+          flash-nvim = {
+            enable = true;
+          };
+        };
+        surround.enable = true;
+        yazi-nvim.enable = true;
+      };
+    };
+  };
+}
--- packages/nvf/package.nix
+++ packages/nvf/package.nix
@@ -5,66 +5,11 @@
   ...
 }: let
   currentFile = /. + __curPos.file;
-  mods = lib.pipe ./. [
+  modules = lib.pipe ./. [
     lib.filesystem.listFilesRecursive
     (lib.filter (f: f != currentFile))
   ];
 in
   (nvf'.lib.neovimConfiguration {
-    inherit pkgs;
-
-    modules =
-      mods
-      ++ [
-        {
-          config = {
-            fzf-lua.enable = true;
-            session.enable = true;
-            vim = {
-              formatter.conform-nvim.enable = true;
-
-              git.enable = true;
-
-              languages = {
-                enableExtraDiagnostics = true;
-                enableFormat = true;
-                enableTreesitter = true;
-                nix.enable = true;
-                nix.extraDiagnostics = {
-                  enable = true;
-                  types = ["statix" "deadnix"];
-                };
-                bash.enable = true;
-              };
-
-              lazy.plugins = {
-                neogit = {
-                  package = pkgs.vimPlugins.neogit;
-                };
-              };
-
-              statusline.lualine.enable = true;
-
-              theme = {
-                enable = true;
-                name = "catppuccin";
-                style = "mocha";
-                transparent = true;
-              };
-
-              ui.borders.enable = true;
-
-              utility = {
-                motion = {
-                  flash-nvim = {
-                    enable = true;
-                  };
-                };
-                surround.enable = true;
-                yazi-nvim.enable = true;
-              };
-            };
-          };
-        }
-      ];
+    inherit pkgs modules;
   }).neovim
