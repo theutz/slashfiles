9f1175f393f7de8bbdbd8e9fd31826900e5a8fa7
Michael Utz <michael@theutz.com>
2025-05-30 12:12:28 +0300

    feat(nvf): add searchBindings to fzf-lua feat(fzf-lua): add searchBindings for additional fzf-lua keymaps

--- packages/nvf/fzf-lua.nix
+++ packages/nvf/fzf-lua.nix
@@ -6,22 +6,37 @@
   inherit (lib) mkIf mkEnableOption;
   inherit (lib.nvim.binds) mkKeymap;
   cfg = config.fzf-lua;
+
+  searchBindings =
+    lib.lists.map (
+      x: {
+        mode = ["n"];
+        key = "<leader>s${lib.elemAt x 0}";
+        action = "<cmd>FzfLua ${lib.elemAt x 1}<cr>";
+        desc = lib.elemAt x 2;
+      }
+    ) [
+      ["b" "buffers" "Open buffers..."]
+    ];
 in {
   options.fzf-lua.enable = mkEnableOption "use fzf-lua";
+
   config.vim = mkIf cfg.enable {
-    keymaps = [
-      (mkKeymap ["n"] "<leader>,"
-        "<cmd>FzfLua buffers<cr>" {desc = "Open buffers...";})
+    keymaps =
+      [
+        (mkKeymap ["n"] "<leader>,"
+          "<cmd>FzfLua buffers<cr>" {desc = "Open buffers...";})
 
-      (mkKeymap ["n"] "<leader>/"
-        "<cmd>FzfLua grep<cr>" {desc = "Search project...";})
+        (mkKeymap ["n"] "<leader>/"
+          "<cmd>FzfLua grep<cr>" {desc = "Search project...";})
 
-      (mkKeymap ["n"] "<leader> "
-        "<cmd>FzfLua files<cr>" {desc = "Open files...";})
+        (mkKeymap ["n"] "<leader> "
+          "<cmd>FzfLua files<cr>" {desc = "Open files...";})
 
-      (mkKeymap ["n"] "<leader>sh"
-        "<cmd>FzfLua helptags<cr>" {desc = "Search help";})
-    ];
+        (mkKeymap ["n"] "<leader>sh"
+          "<cmd>FzfLua helptags<cr>" {desc = "Search help";})
+      ]
+      ++ searchBindings;
 
     fzf-lua = {
       enable = true;
