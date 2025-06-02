afb4327627abe497f463e43f3640438436e91d65
Michael Utz <michael@theutz.com>
2025-06-02 15:11:26 +0300

    refactor(tmux): remove menus import from tmux configuration

--- homes/michael/tmux/default.nix
+++ homes/michael/tmux/default.nix
@@ -49,44 +49,36 @@
       clock24 = true;
       enable = true;
       escapeTime = 0;
-      extraConfig = let
-        menus = lib.pipe (import ./menus.nix) [
-          lib.attrValues
-          lib.concatLines
-        ];
-      in
-        # tmux
-        ''
-          set -g allow-passthrough on
-          set -g allow-rename on
-          set -g default-command "${lib.getExe pkgs.fish}"
-          set -g default-terminal "xterm-256color"
-          set -g extended-keys always
-          set -g terminal-overrides ",xterm*:Tc"
-          set -sa terminal-features "xterm*:extkeys"
-          set -ga update-environment TERM
-          set -ga update-environment TERM_PROGRAM
-          set -ga update-environment PATH
-          set -ga update-environment EDITOR
-          set -ga update-environment VISUAL
+      extraConfig = ''
+        set -g allow-passthrough on
+        set -g allow-rename on
+        set -g default-command "${lib.getExe pkgs.fish}"
+        set -g default-terminal "xterm-256color"
+        set -g extended-keys always
+        set -g terminal-overrides ",xterm*:Tc"
+        set -sa terminal-features "xterm*:extkeys"
+        set -ga update-environment TERM
+        set -ga update-environment TERM_PROGRAM
+        set -ga update-environment PATH
+        set -ga update-environment EDITOR
+        set -ga update-environment VISUAL
 
-          ${lib.optionalString hasCatppuccin ''
-            set -g status-left-length 100
-            set -g status-right-length 100
-            set -g status-left ""
-            set -g @catppuccin_flavor 'mocha'
-            set -g @catppuccin_window_status_style "slanted"
-            set -g status-right "#{E:@catppuccin_status_application}"
-            set -ag status-right "#{E:@catppuccin_status_session}"
-            set -ag status-right "#{E:@catppuccin_status_uptime}"
-          ''}
+        ${lib.optionalString hasCatppuccin ''
+          set -g status-left-length 100
+          set -g status-right-length 100
+          set -g status-left ""
+          set -g @catppuccin_flavor 'mocha'
+          set -g @catppuccin_window_status_style "slanted"
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+        ''}
 
-          ${lib.optionalString hasRosePine ''
-            set -g @rose_pine_variant "main"
-          ''}
+        ${lib.optionalString hasRosePine ''
+          set -g @rose_pine_variant "main"
+        ''}
 
-          ${menus}
-        '';
+      '';
       focusEvents = true;
       keyMode = "vi";
       mouse = true;
