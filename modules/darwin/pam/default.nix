fba4d9012483730c003a33ea2bbaa3a804a58ebf
Michael Utz <michael@theutz.com>
2025-06-03 20:26:43 +0300

    refactor(darwin/pam): abstract module creation into a function

--- modules/darwin/pam/default.nix
+++ modules/darwin/pam/default.nix
@@ -5,18 +5,26 @@
   namespace,
   ...
 }: let
-  mod = builtins.baseNameOf ./.;
-  cfg = config.${namespace}.${mod};
-in {
-  options.${namespace}.${mod} = {
-    enable = lib.mkEnableOption "pam security settings";
+  # mod = builtins.baseNameOf ./.;
+  # cfg = config.${namespace}.${mod};
+  mkMod = ns: dir: spec: let
+    mod = builtins.baseNameOf dir;
+    cfg = config.${ns}.${mod};
+    out = spec {inherit cfg;};
+  in {
+    inherit (out) config;
+    options.${ns}.${mod} = out.options;
   };
+in
+  mkMod namespace ./. ({cfg}: {
+    options.enable = lib.mkEnableOption "pam security settings";
 
-  config = lib.mkIf cfg.enable {
-    environment.etc."pam.d/sudo_local".text = ''
-      auth    optional        ${pkgs.pam-reattach}/lib/pam/pam_reattach.so
-      auth    sufficient      pam_tid.so
-    '';
-    security.pam.services.sudo_local.touchIdAuth = true;
-  };
-}
+    config = lib.mkIf cfg.enable {
+      environment.etc."pam.d/sudo_local".text = ''
+        auth    optional        ${pkgs.pam-reattach}/lib/pam/pam_reattach.so
+        auth    sufficient      pam_tid.so
+      '';
+
+      security.pam.services.sudo_local.touchIdAuth = true;
+    };
+  })
