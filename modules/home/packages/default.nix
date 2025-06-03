3113b96b031daf605274f0f12ad6f47505e93186
Michael Utz <michael@theutz.com>
2025-06-03 19:18:53 +0300

    feat(home): add default packages module
    Add a new module to manage default packages for home configuration, including:
    - Communication tools: zoom-us, signal-desktop-bin
    - Utility tools: coreutils, delta, fd, eza, lazygit, procs, ripgrep
    - Media: spotify-player
    - AI tools: aichat
    - Nerd fonts: roboto-mono, blex-mono

---
+++ modules/home/packages/default.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  namespace,
+  config,
+  lib,
+  ...
+}: let
+  inherit (builtins) baseNameOf;
+  inherit (lib) mkIf mkEnableOption;
+  mod = baseNameOf ./.;
+  cfg = config.${namespace}.${mod};
+in {
+  options.${namespace}.${mod} = {
+    enable = mkEnableOption "Default packages";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs;
+      [
+        zoom-us
+        coreutils
+        delta
+        fd
+        eza
+        lazygit
+        procs
+        ripgrep
+        aichat
+        signal-desktop-bin
+        spotify-player
+      ]
+      ++ (with nerd-fonts; [
+        roboto-mono
+        blex-mono
+      ]);
+  };
+}
