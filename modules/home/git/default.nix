6eec0bbbaed540c866fc7a8d2c48fc2b030d86b4
Michael Utz <michael@theutz.com>
2025-06-03 22:01:13 +0300

    feat(git): move lazygit config from user-specific to shared module

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -11,7 +11,10 @@ lib.${namespace}.mkModule {
   options.enable = lib.mkEnableOption "git-based tool options";
 
   config = lib.mkIf cfg.enable {
+    home.shellAliases.lg = "lazygit";
+
     programs.gh.enable = true;
+
     programs.git = {
       enable = true;
       ignores = ["*~" "*.swp" "*.log" ".direnv"];
@@ -19,6 +22,45 @@ lib.${namespace}.mkModule {
       userEmail = "michael@theutz.com";
       delta = {
         enable = true;
+      };
+    };
+
+    programs.lazygit = {
+      enable = true;
+      settings = {
+        customCommands = [
+          {
+            key = "b";
+            context = "files";
+            command = "HK_PROFILE=ai git commit";
+            description = "generate commit message with llm";
+            output = "terminal";
+          }
+        ];
+        disableStartupPopups = true;
+        git = {
+          commit = {
+            signoff = true;
+          };
+          paging = {
+            colorArg = "always";
+            pager = ''
+              delta "$(dark-mode status | grep on && echo "--dark" || echo "--light")" --paging=never
+            '';
+            parseEmoji = true;
+            overrideGpg = true;
+          };
+        };
+        gui = {
+          border = "rounded";
+          expandFocusedSidePanel = true;
+          nerdFontsVersion = "3";
+          showBottomLine = false;
+          showCommandLog = true;
+          showRandomTip = false;
+        };
+        notARepository = "skip";
+        promptToReturnFromSubprocess = false;
       };
     };
   };
