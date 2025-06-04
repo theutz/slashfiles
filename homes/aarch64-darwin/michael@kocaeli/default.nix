fced8e491459ff5695fc318cddec4d5dbd4f9cfa
Michael Utz <michael@theutz.com>
2025-06-04 23:27:53 +0300

    feat(wezterm): configure alt key behavior for composed keys
    Also adds secrets management with agenix for SSH keys.

--- homes/aarch64-darwin/michael@kocaeli/default.nix
+++ homes/aarch64-darwin/michael@kocaeli/default.nix
@@ -142,6 +142,8 @@ args @ {
           config.color_scheme = "rose-pine"
           config.default_prog = { "${lib.getExe pkgs.fish}" }
           config.hide_tab_bar_if_only_one_tab = true
+          config.send_composed_key_when_left_alt_is_pressed = false
+          config.send_composed_key_when_right_alt_is_pressed = true
 
           -- For compatibility with mprocs https://github.com/pvolok/mprocs/issues/165
           -- config.enable_csi_u_key_encoding = true
---
+++ secrets/secrets.nix
@@ -0,0 +1,11 @@
+let
+  theutz = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbChuj1162NTbJx49GrPJC7qc/mBrXHcDNQO1wbNyJ5";
+  users = [theutz];
+
+  kocaeli = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFX1tT+JKfr6NpoZlIXoQMmlJieaaxxD1/fsU7h7Iojq";
+  systems = [kocaeli];
+
+  personal = [theutz kocaeli];
+in {
+  "theutz-id_ed25519.age".publicKeys = personal;
+}
