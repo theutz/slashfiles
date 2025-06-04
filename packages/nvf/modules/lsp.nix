40e3b9423f7184d1125afec8ea5459cfead0deaa
Michael Utz <michael@theutz.com>
2025-06-04 11:45:11 +0300

    refactor(nvf): integrate diagnostics into lsp module with trouble plugin

--- packages/nvf/modules/lsp.nix
+++ packages/nvf/modules/lsp.nix
@@ -1,34 +1,79 @@
-{
-  config.vim.binds.whichKey.register."<leader>c" = "code/lsp";
-  config.vim.binds.whichKey.register."<leader>cw" = "workspace";
+{lib, ...}: {
+  config = {
+    vim = {
+      binds = {
+        whichKey = {
+          register = {
+            "<leader>l" = "lists";
+            "<leader>c" = "code/lsp";
+            "<leader>cw" = "workspace";
+          };
+        };
+      };
 
-  config.vim.lsp = {
-    enable = true;
-    formatOnSave = true;
-    inlayHints.enable = true;
-    lightbulb.enable = true;
-    lspSignature.enable = true;
-    mappings = {
-      addWorkspaceFolder = "<leader>cwa";
-      codeAction = "<leader>ca";
-      documentHighlight = "<leader>ch";
-      format = "<leader>cf";
-      goToDeclaration = "gD";
-      goToDefinition = "gd";
-      goToType = "gy";
-      hover = "K";
-      listDocumentSymbols = "<leader>cs";
-      listImplementations = "<leader>ci";
-      listReferences = "gr";
-      listWorkspaceFolders = "<leader>cwf";
-      listWorkspaceSymbols = "<leader>cws";
-      nextDiagnostic = "]d";
-      openDiagnosticFloat = "<leader>cd";
-      previousDiagnostic = "[d";
-      removeWorkspaceFolder = "<leader>cwr";
-      renameSymbol = "<leader>cr";
-      signatureHelp = "gK";
-      toggleFormatOnSave = "<leader>uf";
+      diagnostics = {
+        enable = true;
+        config = {
+          virtual_lines = true;
+          virtual_text = true;
+          underline = true;
+          signs = {
+            text =
+              lib.mapAttrs' (name: value: {
+                inherit value;
+                name = "vim.diagnostic.severity.${lib.toUpper name}";
+              })
+              {
+                Error = " ";
+                Warn = " ";
+                Hint = " ";
+                Info = " ";
+              };
+          };
+        };
+      };
+
+      lsp = {
+        enable = true;
+        formatOnSave = true;
+        inlayHints.enable = true;
+        lightbulb.enable = true;
+        lspSignature.enable = true;
+        trouble = {
+          enable = true;
+          mappings = {
+            documentDiagnostics = "<leader>ld";
+            locList = "<leader>ll";
+            lspReferences = "<leader>lr";
+            quickfix = "<leader>lq";
+            symbols = "<leader>ls";
+            workspaceDiagnostics = "<leader>lw";
+          };
+        };
+
+        mappings = {
+          addWorkspaceFolder = "<leader>cwa";
+          codeAction = "<leader>ca";
+          documentHighlight = "<leader>cH";
+          format = "<leader>cf";
+          goToDeclaration = "gD";
+          goToDefinition = "gd";
+          goToType = "gy";
+          hover = "K";
+          listDocumentSymbols = "<leader>cs";
+          listImplementations = "<leader>ci";
+          listReferences = "gr";
+          listWorkspaceFolders = "<leader>cwf";
+          listWorkspaceSymbols = "<leader>cws";
+          nextDiagnostic = "]d";
+          openDiagnosticFloat = "<leader>ch";
+          previousDiagnostic = "[d";
+          removeWorkspaceFolder = "<leader>cwr";
+          renameSymbol = "<leader>cr";
+          signatureHelp = "gK";
+          toggleFormatOnSave = "<leader>uf";
+        };
+      };
     };
   };
 }
