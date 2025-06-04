d59beb0c459cafa1c16d72d1889f04daeafc40db
Michael Utz <michael@theutz.com>
2025-06-04 22:42:51 +0300

    feat(starship): add rose-pine theme configuration

--- homes/aarch64-darwin/michael@kocaeli/default.nix
+++ homes/aarch64-darwin/michael@kocaeli/default.nix
@@ -116,6 +116,13 @@ args @ {
 
     starship = {
       enable = true;
+      settings =
+        (builtins.fetchGit {
+          url = "https://github.com/rose-pine/starship";
+          rev = "c6aeb2833e3d563ca3bbffcb4bad09d44bf817ec";
+        })
+        |> lib.getAttr "outPath"
+        |> (p: "${p}/rose-pine.toml" |> lib.fileContents |> builtins.fromTOML);
     };
 
     wezterm = {
