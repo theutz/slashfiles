b843079163300435094b793cae668f88fbe6b47e
Michael Utz <michael@theutz.com>
2025-06-04 20:18:49 +0300

    feat(nvf): disable grep and replace operations in Yazi

--- packages/nvf/modules/yazi.nix
+++ packages/nvf/modules/yazi.nix
@@ -12,6 +12,8 @@
       setupOpts = {
         open_for_directories = true;
         keymaps = {
+          grep_in_directory = false;
+          replace_in_directory = false;
           open_file_in_horizontal_split = "<c-s>";
           change_working_directory = "<c-g>";
         };
