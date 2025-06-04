07a9f0af1b73d69669027e72789daa26e6797d74
Michael Utz <michael@theutz.com>
2025-06-04 21:09:39 +0300

    feat(nvf): add keymappings for blink-cmp and enable noice UI
    Added keymappings for the blink-cmp autocompletion module and enabled the noice UI plugin in nvf configuration.

--- packages/nvf/modules/completion.nix
+++ packages/nvf/modules/completion.nix
@@ -2,6 +2,15 @@
   config.vim.autocomplete = {
     blink-cmp = {
       enable = true;
+      mappings = {
+        close = "<C-e>";
+        complete = "<C-space>";
+        confirm = "<C-y>";
+        next = "<C-n>";
+        previous = "<C-p>";
+        scrollDocsDown = "<C-d>";
+        scrollDocsUp = "<C-u>";
+      };
       setupOpts = {
         signature.enabled = true;
       };
--- packages/nvf/modules/ui.nix
+++ packages/nvf/modules/ui.nix
@@ -5,6 +5,9 @@
         enable = true;
         globalStyle = "rounded";
       };
+      noice = {
+        enable = true;
+      };
     };
   };
 }
