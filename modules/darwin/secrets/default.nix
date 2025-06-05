3f64e2f467a69fc84dd5bdd70a8c437477c8be5d
Michael Utz <michael@theutz.com>
2025-06-05 22:57:47 +0300

    refactor(darwin): convert sops.templates to mergeAttrsList for extensibility

--- modules/darwin/secrets/default.nix
+++ modules/darwin/secrets/default.nix
@@ -16,17 +16,19 @@ lib.${namespace}.mkModule {
     ];
 
     sops = {
-      templates = {
-        "ssh/izmir.conf" = {
-          content = ''
-            Host ${config.sops.placeholder."ssh/hosts/izmir/host"}
-            Hostname ${config.sops.placeholder."ssh/hosts/izmir/hostname"}
-            User ${config.sops.placeholder."ssh/hosts/izmir/user"}
-            IdentityFile ${config.sops.secrets."ssh/users/yesil/priv".path}
-          '';
-          owner = config.system.primaryUser;
-        };
-      };
+      templates = lib.attrsets.mergeAttrsList [
+        {
+          "ssh/izmir.conf" = {
+            content = ''
+              Host ${config.sops.placeholder."ssh/hosts/izmir/host"}
+              Hostname ${config.sops.placeholder."ssh/hosts/izmir/hostname"}
+              User ${config.sops.placeholder."ssh/hosts/izmir/user"}
+              IdentityFile ${config.sops.secrets."ssh/users/yesil/priv".path}
+            '';
+            owner = config.system.primaryUser;
+          };
+        }
+      ];
 
       defaultSopsFile = ../../../secrets.yaml;
 
