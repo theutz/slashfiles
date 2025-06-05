93f916f8c82fdf1f265413d596017240715cb6b8
Michael Utz <michael@theutz.com>
2025-06-05 22:52:39 +0300

    feat(sops): implement mkSshConf template for izmir host

--- lib/secrets.nix
+++ lib/secrets.nix
@@ -1,7 +1,16 @@
 {
   sops = {
     templates = {
-      mkSshConf = {config, ...}: {};
+      mkSshConf = {config, ...}: {
+        "ssh/izmir.conf" = {
+          content = ''
+            Host ${config.sops.placeholder."ssh/hosts/izmir/host"}
+            Hostname ${config.sops.placeholder."ssh/hosts/izmir/hostname"}
+            User ${config.sops.placeholder."ssh/hosts/izmir/user"}
+            IdentityFile ${config.sops.secrets."ssh/users/yesil/priv".path}
+          '';
+          owner = config.system.primaryUser;
+      };
     };
   };
 }
