6554ee01b82f74046051233f71db2119fb83673b
Michael Utz <michael@theutz.com>
2025-06-05 23:02:26 +0300

    fix(darwin): remove duplicate SSH config for Izmir

--- modules/darwin/secrets/default.nix
+++ modules/darwin/secrets/default.nix
@@ -20,17 +20,6 @@ in
 
       sops = {
         templates = lib.attrsets.mergeAttrsList [
-          {
-            "ssh/izmir.conf" = {
-              content = ''
-                Host ${config.sops.placeholder."ssh/hosts/izmir/host"}
-                Hostname ${config.sops.placeholder."ssh/hosts/izmir/hostname"}
-                User ${config.sops.placeholder."ssh/hosts/izmir/user"}
-                IdentityFile ${config.sops.secrets."ssh/users/yesil/priv".path}
-              '';
-              owner = config.system.primaryUser;
-            };
-          }
           (mkSshConf {
             inherit config;
             host = "izmir";
