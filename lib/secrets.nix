17f29ebe24b3a5541381ec72c36e3b20acf8061a
Michael Utz <michael@theutz.com>
2025-06-05 23:20:24 +0300

    refactor(secrets): refactor SSH config generation using functional approach

--- lib/secrets.nix
+++ lib/secrets.nix
@@ -1,4 +1,4 @@
-_: {
+{lib, ...}: {
   sops = {
     templates = {
       mkSshConf' = config: {
@@ -6,12 +6,21 @@ _: {
         id,
       }: {
         "ssh/${host}.conf" = {
-          content = ''
-            Host ${config.sops.placeholder."ssh/hosts/${host}/host"}
-            Hostname ${config.sops.placeholder."ssh/hosts/${host}/hostname"}
-            User ${config.sops.placeholder."ssh/hosts/${host}/user"}
-            IdentityFile ${config.sops.secrets."ssh/users/${id}/priv".path}
-          '';
+          content = let
+            inherit (config.sops) secrets placeholder;
+          in
+            [
+              (let label = "ssh/hosts/${host}/host"; in lib.optionalAttrs (placeholder ? label) {Host = placeholder.${label};})
+            ]
+            |> lib.attrsets.mergeAttrsList
+            |> lib.attrsets.mapAttrsToList (name: value: "${name} ${value}")
+            |> lib.strings.concatLines;
+          # content = ''
+          #   Host ${config.sops.placeholder."ssh/hosts/${host}/host"}
+          #   Hostname ${config.sops.placeholder."ssh/hosts/${host}/hostname"}
+          #   User ${config.sops.placeholder."ssh/hosts/${host}/user"}
+          #   IdentityFile ${config.sops.secrets."ssh/users/${id}/priv".path}
+          # '';
           owner = config.system.primaryUser;
         };
       };
