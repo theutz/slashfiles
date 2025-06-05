824c459d1dbe715f08b40b2e4a8f5ef9f8cff68a
Michael Utz <michael@theutz.com>
2025-06-05 15:18:01 +0300

    feat(tmux): add create menu for creating panes, windows, and sessions

--- modules/home/tmux/menus.nix
+++ modules/home/tmux/menus.nix
@@ -167,8 +167,181 @@ in {
         '')
       ];
     })
+    (mkMenu {
+      name = "create";
+      key = "e";
+      mkItems = {
+        divider,
+        mkItem,
+      }: [
+        (mkItem "j" "Pane below" ''
+          command-prompt -p "New pane name:,New pane command:" \
+            -I '#T,#{default-shell}' {
+              split-window -v '%2'
+              select-pane -T '%1'
+            }
+        '')
+        (mkItem "k" "Pane above" ''
+          command-prompt -p "New pane name:,New pane command:" \
+            -I '#T,#{default-shell}' {
+              split-window -v -b '%2'
+              select-pane -T '%1'
+            }
+        '')
+        (mkItem "l" "Pane right" ''
+          command-prompt -p "New pane name:,New pane command:" \
+            -I '#T,#{default-shell}' {
+              split-window -h '%2'
+              select-pane -T '%1'
+            }
+        '')
+        (mkItem "h" "Pane left" ''
+          command-prompt -p "New pane name:,New pane command:" \
+            -I '#T,#{default-shell}' {
+              split-window -h -b '%2'
+              select-pane -T '%1'
+            }
+        '')
+        divider
+        (mkItem "J" "Pane below all" ''
+          command-prompt -p "New pane name:,New pane command:" \
+            -I '#T,#{default-shell}' {
+              split-window -v -f '%2'
+              select-pane -T '%1'
+            }
+        '')
+        (mkItem "K" "Pane above all" ''
+          command-prompt -p "New pane name:,New pane command:" \
+            -I '#T,#{default-shell}' {
+              split-window -v -f -b '%2'
+              select-pane -T '%1'
+            }
+        '')
+        (mkItem "L" "Pane far right" ''
+          command-prompt -p "New pane name:,New pane command:" \
+            -I '#T,#{default-shell}' {
+              split-window -h -f '%2'
+              select-pane -T '%1'
+            }
+        '')
+        (mkItem "H" "Pane far left" ''
+          command-prompt -p "New pane name:,New pane command:" \
+            -I '#T,#{default-shell}' {
+              split-window -h -f -b '%2'
+              select-pane -T '%1'
+            }
+        '')
+        divider
+        (mkItem "w" "New window (end)" ''
+          command-prompt -p "New window name:,New window command:,New pane name:" \
+            -I '#W,#{default-shell},#T' {
+              new-window -n '%1' %2
+              select-pane -T '%3'
+            }
+        '')
+        (mkItem "n" "New window (next)" ''
+          command-prompt -p "New window name:,New window command:,New pane name:" \
+            -I '#W,#{default-shell},#T' {
+              new-window -a -n '%1' %2
+              select-pane -T '%3'
+            }
+        '')
+        (mkItem "p" "New window (prev)" ''
+          command-prompt -p "New window name:,New window command:,New pane name:" \
+            -I '#W,#{default-shell},#T' {
+              new-window -b -n '%1' %2
+              select-pane -T '%3'
+            }
+        '')
+        divider
+        (mkItem "s" "New session" ''
+          command-prompt -p "New session name:,New window name:,New pane command:,New pane name:" \
+            -I '#S,#W,#{default-shell},#T' {
+              new-session -s '%1' -n '%2' '%3'
+              select-pane -T '%4'
+            }
+        '')
+      ];
+    })
+
+    # create = ''
+    #   bind-key -N "Open create menu" e \
+    #     display-menu -T "Create..." -x "#{popup_pane_left}" -y "#{popup_pane_bottom}" \
+    #       "Pane below" j {
+    #         command-prompt -p "New pane name:,New pane command:" \
+    #           -I '#T,#{default-shell}' {
+    #             split-window -v '%2'
+    #             select-pane -T '%1'
+    #           }
+    #       } "Pane above" k {
+    #         command-prompt -p "New pane name:,New pane command:" \
+    #           -I '#T,#{default-shell}' {
+    #             split-window -v -b '%2'
+    #             select-pane -T '%1'
+    #           }
+    #       } "Pane right" l {
+    #         command-prompt -p "New pane name:,New pane command:" \
+    #           -I '#T,#{default-shell}' {
+    #             split-window -h '%2'
+    #             select-pane -T '%1'
+    #           }
+    #       } "Pane left" h {
+    #         command-prompt -p "New pane name:,New pane command:" \
+    #           -I '#T,#{default-shell}' {
+    #             split-window -h -b '%2'
+    #             select-pane -T '%1'
+    #           }
+    #       } ''' "Pane below all" J {
+    #         command-prompt -p "New pane name:,New pane command:" \
+    #           -I '#T,#{default-shell}' {
+    #             split-window -v -f '%2'
+    #             select-pane -T '%1'
+    #           }
+    #       } "Pane above all" K {
+    #         command-prompt -p "New pane name:,New pane command:" \
+    #           -I '#T,#{default-shell}' {
+    #             split-window -v -f -b '%2'
+    #             select-pane -T '%1'
+    #           }
+    #       } "Pane far right" L {
+    #         command-prompt -p "New pane name:,New pane command:" \
+    #           -I '#T,#{default-shell}' {
+    #             split-window -h -f '%2'
+    #             select-pane -T '%1'
+    #           }
+    #       } "Pane far left" H {
+    #         command-prompt -p "New pane name:,New pane command:" \
+    #           -I '#T,#{default-shell}' {
+    #             split-window -h -f -b '%2'
+    #             select-pane -T '%1'
+    #           }
+    #       } ''' "New window (end)" w {
+    #         command-prompt -p "New window name:,New window command:,New pane name:" \
+    #           -I '#W,#{default-shell},#T' {
+    #             new-window -n '%1' %2
+    #             select-pane -T '%3'
+    #           }
+    #       } "New window (next)" n {
+    #         command-prompt -p "New window name:,New window command:,New pane name:" \
+    #           -I '#W,#{default-shell},#T' {
+    #             new-window -a -n '%1' %2
+    #             select-pane -T '%3'
+    #           }
+    #       } "New window (prev)" p {
+    #         command-prompt -p "New window name:,New window command:,New pane name:" \
+    #           -I '#W,#{default-shell},#T' {
+    #             new-window -b -n '%1' %2
+    #             select-pane -T '%3'
+    #           }
+    #       } ''' "New session" s {
+    #         command-prompt -p "New session name:,New window name:,New pane command:,New pane name:" \
+    #           -I '#S,#W,#{default-shell},#T' {
+    #             new-session -s '%1' -n '%2' '%3'
+    #             select-pane -T '%4'
+    #           }
+    #       }
+    # '';
 
     layout
-    create
   ];
 }
