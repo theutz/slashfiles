a54e078a0d672ba69b5e467e9bb48c19e9edc444
Michael Utz <michael@theutz.com>
2025-06-10 15:01:27 +0300

    feat(nvf): add treewalker keybindings for movement and swapping

--- packages/nvf/modules/treesitter.nix
+++ packages/nvf/modules/treesitter.nix
@@ -1,9 +1,35 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.nvim.binds) mkKeymap;
+  mkMove = key: dir: (mkKeymap ["n" "v"] key "<cmd>Treewalker ${dir}<cr>" {
+    desc = "Treewalker move ${dir}";
+    silent = true;
+  });
+  mkSwap = key: dir: (mkKeymap "n" key "<cmd>Treewalker Swap${dir}<cr>" {
+    desc = "Treewalker swap ${dir}";
+    silent = true;
+  });
+in {
   config.vim.lazy.plugins = {
     "treewalker.nvim" = {
       package = pkgs.vimPlugins.treewalker-nvim;
+      keys = [
+        (mkMove "A-h" "Left")
+        (mkMove "A-j" "Down")
+        (mkMove "A-k" "Up")
+        (mkMove "A-l" "Right")
+
+        (mkSwap "C-A-h" "Left")
+        (mkSwap "C-A-j" "Down")
+        (mkSwap "C-A-k" "Up")
+        (mkSwap "C-A-l" "Right")
+      ];
     };
   };
+
   config.vim.treesitter = {
     addDefaultGrammars = true;
     autotagHtml = true;
