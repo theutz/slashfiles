e09336106a360b3ca77957ecf3fa8b2089f16693
Michael Utz <michael@theutz.com>
2025-06-16 15:30:48 +0300

    feat(nvf): add CodeCompanionChat keymap for codecompanion-nvim

--- packages/nvf/modules/assistant.nix
+++ packages/nvf/modules/assistant.nix
@@ -1,5 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib.nvim.binds) mkKeymap;
+in {
   config.vim = {
+    keymaps = lib.optionals config.vim.assistant.codecompanion-nvim.enable [
+      (mkKeymap "n" "<leader>clc" "<cmd>CodeCompanionChat<cr>" {desc = "Chat";})
+    ];
+
     assistant = {
       codecompanion-nvim = {
         enable = true;
