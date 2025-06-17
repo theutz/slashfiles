b1da569671c0757d88ad4a5d87a13b8c9e06eabe
Michael Utz <michael@theutz.com>
2025-06-17 15:13:13 +0300

    fix(direnv): correct watch_dir pattern and enhance watch command logging

--- shells/slashfiles/watch.nix
+++ shells/slashfiles/watch.nix
@@ -10,44 +10,85 @@ in
       watchexec
       gum
       pkgs.slashfiles.swch
+      getopt
     ];
 
     text =
       # bash
       ''
+        function log() {
+          gum log --prefix "${name}" "$@"
+        }
+
+        function debug() {
+          if [[ $flag_verbose == y ]]; then
+            log --level debug "$@"
+          fi
+        }
+
+        function info() {
+          log --level info "$@"
+        }
+
+        function error() {
+          log --level error "$@"
+        }
+
         function usage() {
           cat <<-markdown | gum format
         # ${name}
 
         ${description}
 
-        All other flags and arguments are passed to 'swch'
+        ## USAGE
 
-        FLAGS
+        > ${name} [flags]
+
+        ## FLAGS
+
+        -d, --debug        Enable debug logging
         -h, --help         Show this help
         -i, --immediate    Run on first invocation
+        -v, --verbose      Enable verbose logging
+
+        NOTE: All other flags and arguments are passed to 'swch'
         markdown
         }
 
         parsed="$(getopt \
-          --longoptions help,immediate \
-          --options hi \
+          --longoptions=debug,help,immediate,verbose \
+          --options=dhiv \
           --name "${name}" \
           -- "$@"
         )" || exit 2
-        eval set -- "$parsed"
+        eval set -- "''$parsed"
 
-        flag_help=n flag_immediate=n
+        flag_help=n flag_immediate=n flag_verbose=n flag_debug=n
 
         while [[ $# -gt 0 ]]; do
           case "$1" in
+            --debug | -d) flag_debug=y;;
             --help | -h) flag_help=y;;
             --immediate | -i) flag_immediate=y;;
+            --verbose | -v) flag_verbose=y;;
             --) shift; break;;
             *) error "Programming error"; exit 1;;
           esac
           shift
         done
+
+        if [[ $flag_debug == y ]]; then
+          set -x
+          flag_verbose=y
+          debug "Debug mode enabled"
+        fi
+
+        if [[ $flag_verbose == y ]]; then
+          exec 3>&1
+          debug "Verbose mode enabled"
+        else
+          exec 3>/dev/null
+        fi
 
         if [[ $flag_help == y ]]; then
           usage
