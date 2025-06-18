af168cf16b18447c76cdaba5c27efc10b6b34992
Michael Utz <michael@theutz.com>
2025-06-18 17:15:08 +0300

    feat(wezterm): accept namespace parameter for preference access

--- modules/home/wezterm/default.nix
+++ modules/home/wezterm/default.nix
@@ -2,33 +2,36 @@
   lib,
   config,
   pkgs,
+  namespace,
   ...
-}:
-lib.slashfiles.mkModule {
-  inherit config;
-  here = ./.;
-}
-{
-  config = {
-    programs.wezterm = {
-      enable = true;
+}: let
+  inherit (lib.${namespace}) prefs;
+in
+  lib.slashfiles.mkModule {
+    inherit config;
+    here = ./.;
+  }
+  {
+    config = {
+      programs.wezterm = {
+        enable = true;
 
-      enableBashIntegration = true;
-      enableZshIntegration = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
 
-      extraConfig =
-        {
-          fish = lib.getExe pkgs.fish;
-          font-family = lib.slashfiles.prefs.font.family;
-          font-size = lib.slashfiles.prefs.font.size;
-          line-height = lib.slashfiles.prefs.font.height;
-          dark-theme = lib.slashfiles.prefs.theme.dark.wezterm;
-          light-theme = lib.slashfiles.prefs.theme.light.wezterm;
-          opacity = 0.85;
-        }
-        |> pkgs.replaceVars ./wezterm.lua
-        |> builtins.toPath
-        |> lib.fileContents;
+        extraConfig =
+          {
+            fish = lib.getExe pkgs.fish;
+            font-family = prefs.font.family;
+            font-size = prefs.font.size;
+            line-height = prefs.font.height;
+            dark-theme = prefs.theme.dark.wezterm;
+            light-theme = prefs.theme.light.wezterm;
+            opacity = 0.85;
+          }
+          |> pkgs.replaceVars ./wezterm.lua
+          |> builtins.toPath
+          |> lib.fileContents;
+      };
     };
-  };
-}
+  }
