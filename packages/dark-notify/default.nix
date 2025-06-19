0a0d03a1f494f88ce667307d99cc60345507f463
Michael Utz <michael@theutz.com>
2025-06-19 21:39:50 +0300

    feat(packages): add dark-notify package for macOS light/dark mode changes

---
+++ packages/dark-notify/default.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "dark-notify";
+  version = "master";
+
+  meta = {
+    description = ''
+      Watcher for macOS 10.14+ light/dark mode changes
+    '';
+    homepage = "https://github.com/cormacrelf/dark-notify";
+    license = [];
+    maintainers = [];
+    platforms = lib.platforms.darwin;
+  };
+
+  src = pkgs.fetchFromGitHub {
+    owner = "cormacrelf";
+    repo = "dark-notify";
+    rev = "0d8501ca027b4355ed958b937ed51b37632c60cf";
+    hash = "sha256-r71Xe8nBaio0+DP4Q+tljtACPvKlT4URbBS5qq58msE=";
+  };
+
+  cargoHash = "sha256-xICF/BAu+Gbg9rXhPgu2NKxQyFAuSGCPYEHAUsUCWrY=";
+}
