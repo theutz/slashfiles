646a106e270106109c1763050507ca3845c5cc90
Michael Utz <michael@theutz.com>
2025-06-19 17:02:32 +0300

    feat(bat): customize theme based on preferences

--- modules/home/bat/default.nix
+++ modules/home/bat/default.nix
@@ -1,32 +1,47 @@
 {
   pkgs,
+  namespace,
   lib,
   config,
   ...
-}:
-lib.slashfiles.mkModule {
-  inherit config;
-  here = ./.;
-} {
-  config = {
-    home.shellAliases = {
-      cat = "bat";
-    };
-    programs.bat = {
-      enable = true;
-      config = {theme = "ansi";};
-      extraPackages = with pkgs.bat-extras; [batman batwatch batdiff batgrep];
-      syntaxes = {
-        tmux = {
-          src = pkgs.fetchFromGitHub {
-            owner = "gerardroche";
-            repo = "sublime-tmux";
-            rev = "c7c6891698b752d5c6050929e4896bb8caa608ae";
-            hash = "sha256-c7WJOmrYi8MLCU19O8KGNfV7YxSO+SdVmxtwsdkIxtQ=";
+}: let
+  themes = rec {
+    rose-pine = "ansi";
+    rose-pine-moon = rose-pine;
+    rose-pine-dawn = rose-pine;
+  };
+  inherit (lib.${namespace}.prefs.theme) main;
+  theme = lib.getAttr main themes;
+in
+  lib.slashfiles.mkModule {
+    inherit config;
+    here = ./.;
+  } {
+    config = {
+      home.shellAliases = {
+        cat = "bat";
+      };
+
+      programs.bat = {
+        enable = true;
+
+        config = {
+          inherit theme;
+        };
+
+        extraPackages = with pkgs.bat-extras; [batman batwatch batdiff batgrep];
+
+        syntaxes = {
+          tmux = {
+            src = pkgs.fetchFromGitHub {
+              owner = "gerardroche";
+              repo = "sublime-tmux";
+              rev = "c7c6891698b752d5c6050929e4896bb8caa608ae";
+              hash = "sha256-c7WJOmrYi8MLCU19O8KGNfV7YxSO+SdVmxtwsdkIxtQ=";
+            };
+            file = "Tmux.sublime-syntax";
           };
-          file = "Tmux.sublime-syntax";
         };
       };
     };
-  };
-}
+  }
