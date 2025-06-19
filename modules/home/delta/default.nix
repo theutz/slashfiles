89dcbb157afaeb17aca275e436b54121f7a4e52a
Michael Utz <michael@theutz.com>
2025-06-19 19:42:17 +0300

    feat: refactor delta module and add tmux overlay

--- modules/home/delta/default.nix
+++ modules/home/delta/default.nix
@@ -3,33 +3,33 @@
   config,
   namespace,
   ...
-}: let
-  themes = rec {
-    rose-pine = "ansi";
-    rose-pine-moon = rose-pine;
-    rose-pine-dawn = rose-pine;
-  };
-  inherit (lib.${namespace}.prefs.theme) main;
-  theme = lib.getAttrs main themes;
-in
-  lib.slashfiles.mkModule {
-    inherit config;
-    here = ./.;
-  } {
-    config = {
-      programs.git.delta = {
-        enable = config.${namespace}.git.enable;
-        options = {};
+}:
+lib.slashfiles.mkModule {
+  inherit config;
+  here = ./.;
+} {
+  config = {
+    programs.git.delta = {
+      enable = config.${namespace}.git.enable;
+      options = {
+        dark =
+          {
+            rose-pine = true;
+            rose-pine-moon = true;
+            rose-pine-dawn = false;
+          }
+          |> lib.getAttr lib.${namespace}.prefs.theme.main;
       };
-
-      programs.lazygit.settings.git.paging.pager =
-        # bash
-        ''
-          delta "$(dark-mode status | grep on && echo "--dark" || echo "--light")" --paging=never
-        '';
-
-      programs.tmux.extraConfig = ''
-        set -ga terminal-overrides ",*-256color:Tc"
-      '';
     };
-  }
+
+    programs.lazygit.settings.git.paging.pager =
+      # bash
+      ''
+        delta "$(dark-mode status | grep on && echo "--dark" || echo "--light")" --paging=never
+      '';
+
+    programs.tmux.extraConfig = ''
+      set -ga terminal-overrides ",*-256color:Tc"
+    '';
+  };
+}
---
+++ overlays/tmux/default.nix
@@ -0,0 +1,12 @@
+{channels, ...}: _: _prev: {
+  tmux =
+    channels.unstable.tmux
+    // {
+      src = channels.unstable.fetchFromGitHub {
+        owner = "tmux";
+        repo = "tmux";
+        rev = "43e88c892d41181088b2ff7908e2f2467fd30916";
+        hash = "";
+      };
+    };
+}
