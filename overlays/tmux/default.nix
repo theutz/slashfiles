e044c85b163d3a19f2aad4f32f1e84999841e9b3
Michael Utz <michael@theutz.com>
2025-06-20 01:04:37 +0300

    fix(tmux): update fetch hash for custom tmux build

--- overlays/tmux/default.nix
+++ overlays/tmux/default.nix
@@ -1,12 +1,12 @@
 {channels, ...}: _: _: {
   tmux =
-    channels.unstable.tmux
-    // {
+    channels.unstable.tmux.overrideAttrs
+    {
       src = channels.unstable.fetchFromGitHub {
         owner = "tmux";
         repo = "tmux";
         rev = "43e88c892d41181088b2ff7908e2f2467fd30916";
-        hash = "";
+        hash = "sha256-RCZfUnUHQuPrjPE01SMeVvT772Ob5xqaGboxMCjOIKg=";
       };
     };
 }
