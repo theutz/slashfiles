ffc0ee5d679145baece82475c529cccd943f1624
Michael Utz <michael@theutz.com>
2025-06-19 16:54:28 +0300

    feat(home/bat): add extra packages and tmux syntax highlight

--- modules/home/bat/default.nix
+++ modules/home/bat/default.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
@@ -14,6 +15,18 @@ lib.slashfiles.mkModule {
     programs.bat = {
       enable = true;
       config = {theme = "ansi";};
+      extraPackages = with pkgs.bat-extras; [batman batwatch batdiff batgrep];
+      syntaxes = {
+        tmux = {
+          src = pkgs.fetchFromGitHub {
+            owner = "kei-q";
+            repo = "sublime-tmux-syntax-highlight";
+            rev = "9162993af1eab09413cd141a640cbffe7122d1de";
+            hash = "sha256-B2gypmQh1GWh/0UaD+BFbBumGhIYmrq0KpCAkgrK4ws=";
+          };
+          file = "Tmux.tmLanguage";
+        };
+      };
     };
   };
 }
