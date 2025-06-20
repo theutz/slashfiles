1d26b28f4806e4951a971e47eab04406145c3d01
Michael Utz <michael@theutz.com>
2025-06-20 22:47:26 +0300

    feat(home/wthrr): configure weather crab module

--- modules/home/wthrr/default.nix
+++ modules/home/wthrr/default.nix
@@ -2,6 +2,7 @@
   namespace,
   lib,
   config,
+  pkgs,
   ...
 }: let
   mod = baseNameOf ./.;
@@ -10,7 +11,75 @@ in {
   options.${namespace}.${mod} = {
     # https://github.com/ttytm/wthrr-the-weathercrab
     enable = lib.mkEnableOption "enable wthrr - the weather crab";
+
+    address = lib.mkOption {
+      type = with lib.types; uniq str;
+      default = "Golcuk, Kocaeli, TR";
+      description = ''
+        Address to check the weather, e.g.: "Berlin,DE"
+      '';
+    };
+
+    language = lib.mkOption {
+      type = with lib.types; uniq str;
+      description = ''
+        Language code of the output language
+      '';
+      default = "en_US";
+    };
+
+    forecast = lib.mkOption {
+      type = with lib.types;
+        uniq (listOf (enum [
+          "week"
+          "day"
+          "today"
+          "tomorrow"
+          "mo"
+          "tu"
+          "we"
+          "th"
+          "fr"
+          "sa"
+          "su"
+          "disable"
+        ]));
+      description = ''
+        Forecast to display without adding the `-f` option: `[day]` | `[week]` | `[day, week]`
+      '';
+      default = ["day" "week"];
+    };
   };
 
-  config.${namespace}.${mod} = lib.mkIf cfg.enable {};
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [wthrr];
+
+    home.file."Application Support/weathercrab/wthrr.ron" = {
+      text = ''
+        (
+            address: "${cfg.address}",
+            language: "${cfg.language}",
+            forecast: [
+              ${cfg.forecast |> lib.concatStringsSep ",\n"}
+            ],
+            units: (
+                temperature: celsius,
+                speed: kmh,
+                time: military,
+                precipitation: probability,
+            ),
+            gui: (
+                border: rounded,
+                color: default,
+                graph: (
+                    style: lines(solid),
+                    rowspan: double,
+                    time_indicator: true,
+                ),
+                greeting: true,
+            ),
+        )
+      '';
+    };
+  };
 }
