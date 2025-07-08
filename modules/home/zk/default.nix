8fdd545d64aa687ca3e516037f0ec0c9f332bafb
Michael Utz <michael@theutz.com>
2025-07-08 15:42:27 -0700

    feat(home/zk): add emanote note-taking app and launchd service

--- modules/home/zk/default.nix
+++ modules/home/zk/default.nix
@@ -2,10 +2,13 @@
   config,
   namespace,
   lib,
+  pkgs,
   ...
 }: let
   mod = baseNameOf ./.;
   cfg = config.${namespace}.${mod};
+
+  Label = "com.${namespace}.${mod}.emanote";
 in {
   options.${namespace}.${mod} = {
     enable = lib.mkEnableOption "enable ${mod}";
@@ -16,7 +19,7 @@ in {
       enable = true;
       settings = {
         notebook = {
-          dir = "~/notes";
+          dir = config.home.homeDirectory + "/notes";
         };
         notes = {
           language = "en";
@@ -28,6 +31,25 @@ in {
           id-length = 4;
           id-case = "lower";
         };
+      };
+    };
+
+    home.packages = with pkgs; [emanote];
+
+    launchd.agents.emanote = {
+      enable = true;
+      config = {
+        inherit Label;
+        RunAtLoad = true;
+        KeepAlive = true;
+        StandardOutPath = "/tmp/${Label}/out.log";
+        StandardErrorPath = "/tmp/${Label}/err.log";
+        WorkingDirectory = config.programs.zk.settings.notebook.dir;
+        ProgramArguments = [
+          (lib.getExe pkgs.emanote)
+          "run"
+          "--port=3842"
+        ];
       };
     };
   };
