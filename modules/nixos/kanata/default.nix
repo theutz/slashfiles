5b1927fdfc623bcb209f52f9bf3a429ff6753c41
Michael Utz <michael@theutz.com>
2025-07-14 15:17:49 -0700

    chore: fix hold time for kanata

--- modules/nixos/kanata/default.nix
+++ modules/nixos/kanata/default.nix
@@ -28,7 +28,7 @@ in {
               hold-time 200
             )
             (defalias
-              caps (tap-hold $tap-time $hold-time esc lctl)
+              caps (tap-hold 200 100 esc lctl)
               tab (tap-hold $tap-time $hold-time tab (layer-toggle arrow))
               del del ;; alias for the true delete key action
             )
