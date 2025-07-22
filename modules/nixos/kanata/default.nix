73399dd8c4d9868502616fa9a79e48583866ac1b
Michael Utz <michael@theutz.com>
2025-07-22 19:29:02 +0300

    chore(kanata): spacing

--- modules/nixos/kanata/default.nix
+++ modules/nixos/kanata/default.nix
@@ -19,26 +19,32 @@ in {
           extraDefCfg = ''
             process-unmapped-keys yes
           '';
+
           config = ''
             (defsrc
               caps tab d h j k l
             )
+
             (defvar
               tap-time 200
               hold-time 200
             )
+
             (defalias
               caps (tap-hold 100 150 esc lctl)
               tab (tap-hold $tap-time $hold-time tab (layer-toggle arrow))
               del del ;; alias for the true delete key action
             )
+
             (deflayer default
               @caps @tab d h j k l
             )
+
             (deflayer arrow
               _ _ @del left down up right
             )
           '';
+
         };
       };
     };
