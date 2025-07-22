3a7999fc3e687577bd4fd6bfcb4d63545bc340ad
Michael Utz <michael@theutz.com>
2025-07-22 19:28:25 +0300

    feat(home,hyprland): more default settings

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -24,17 +24,26 @@ in {
       enable = true;
       settings = {
         monitor = [", preferred, auto, auto"];
+
+        xwayland = {
+          force_zero_scaling = true;
+        };
+
         "$terminal" = "wezterm";
         "$fileManager" = "dolphin";
         "$menu" = "wofi --show drun";
 
         exec-once = [
           "$terminal"
+          "waybar"
         ];
+
         env = [
+          "GDK_SCALE,2"
           "XCURSOR_SIZE,24"
           "HYPRCURSOR_SIZE,24"
         ];
+
         general = {
           gaps_in = 5;
           gaps_out = 20;
@@ -51,15 +60,95 @@ in {
           layout = "dwindle";
         };
 
+        decoration = {
+          rounding = 10;
+          rounding_power = 2;
+
+          active_opacity = 1.0;
+          inactive_opacity = 1.0;
+
+          shadow = {
+            enabled = true;
+            range = 4;
+            render_power = 3;
+            color = "rgba(1a1a1aee)";
+          };
+
+          blur = {
+            enabled = true;
+            size = 3;
+            passes = 1;
+            vibrancy = 0.1696;
+          };
+        };
+
+        animations = {
+          bezier = [
+            "easeOutQuint,0.23,1,0.32,1"
+            "easeInOutCubic,0.65,0.05,0.36,1"
+            "linear,0,0,1,1"
+            "almostLinear,0.5,0.5,0.75,1.0"
+            "quick,0.15,0,0.1,1"
+          ];
+
+          animation = [
+            "global, 1, 10, default"
+            "border, 1, 5.39, easeOutQuint"
+            "windows, 1, 4.79, easeOutQuint"
+            "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+            "windowsOut, 1, 1.49, linear, popin 87%"
+            "fadeIn, 1, 1.73, almostLinear"
+            "fadeOut, 1, 1.46, almostLinear"
+            "fade, 1, 3.03, quick"
+            "layers, 1, 3.81, easeOutQuint"
+            "layersIn, 1, 4, easeOutQuint, fade"
+            "layersOut, 1, 1.5, linear, fade"
+            "fadeLayersIn, 1, 1.79, almostLinear"
+            "fadeLayersOut, 1, 1.39, almostLinear"
+            "workspaces, 1, 1.94, almostLinear, fade"
+            "workspacesIn, 1, 1.21, almostLinear, fade"
+            "workspacesOut, 1, 1.94, almostLinear, fade"
+          ];
+        };
+
+        dwindle = {
+          pseudotile = true;
+          preserve_split = true;
+        };
+
+        master = {
+          new_status = "master";
+        };
+
+        misc = {
+          force_default_wallpaper = -1;
+          disable_hyprland_logo = false;
+        };
+
+        input = {
+          kb_layout = "us";
+          follow_mouse = 1;
+          sensitivity = 0;
+
+          touchpad = {
+            natural_scroll = true;
+          };
+        };
+
+        gestures = {
+          workspace_swipe = false;
+        };
+
         "$mainMod" = "SUPER";
 
         bind = [
           "$mainMod, M, exec, $terminal"
+          "$mainMod, B, exec, qutebrowser"
           "$mainMod, C, killactive,"
           "$mainMod, Q, exit,"
           "$mainMod, E, exec, $fileManager"
           "$mainMod, Z, togglefloating,"
-          "$mainMod, R, exec, $menu"
+          "$mainMod, space, exec, $menu"
           "$mainMod, P, pseudo, # dwindle"
           "$mainMod, V, togglesplit, # dwindle"
 
@@ -88,6 +177,17 @@ in {
           "$mainMod SHIFT, 8, movetoworkspace, 8"
           "$mainMod SHIFT, 9, movetoworkspace, 9"
           "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+          "$mainMod, S, togglespecialworkspace, magic"
+          "$mainMod SHIFT, S, movetoworkspace, special:magic"
+
+          "$mainMod, mouse_down, workspace, e+1"
+          "$mainMod, mouse_up, workspace, e-1"
+        ];
+
+        bindm = [
+          "$mainMod, mouse:272, movewindow"
+          "$mainMod, mouse:273, resizewindow"
         ];
 
         windowrule = [
@@ -95,6 +195,10 @@ in {
           "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
         ];
       };
+    };
+
+    programs.waybar = {
+      enable = true;
     };
   };
 }
