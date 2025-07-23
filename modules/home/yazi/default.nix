30c8c717ba3e543413965e45c3edbdf4d32be21a
Michael Utz <michael@theutz.com>
2025-07-23 15:10:39 +0300

    refactor(home,yazi): create module directly

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -4,12 +4,13 @@
   namespace,
   config,
   ...
-}:
-lib.${namespace}.mkModule {
-  inherit config;
-  here = ./.;
-} {
-  config = {
+}: let
+  mod = builtins.baseNameOf ./.;
+  cfg = config.${namespace}.${mod};
+in {
+  options.${namespace}.${mod}.enable = lib.mkEnableOption "enable ${mod}";
+
+  config = lib.mkIf cfg.enable {
     home.shellAliases = {
       y = "y";
     };
