f671c8ca0989de90b55635aebd7120d8ffc244db
Michael Utz <michael@theutz.com>
2025-07-30 19:54:32 +0300

    chore: cleanup git stuff

--- nix/homes/x86_64-linux/michael@konya/default.nix
+++ nix/homes/x86_64-linux/michael@konya/default.nix
@@ -13,29 +13,37 @@
       enable = true;
       enableWorkstation = true;
     };
+
     hyprland.monitor = [
       "eDP-1, preferred, auto, 1"
       "HDMI-A-1, preferred, auto-left, 1.25"
     ];
   };
 
-  programs.wofi.enable = true;
+  programs = {
+    wofi.enable = true;
 
-  programs.zellij.enable = true;
+    zellij.enable = true;
 
-  programs.tmux.enable = true;
+    tmux.enable = true;
 
-  programs.home-manager.enable = true;
+    home-manager.enable = true;
 
-  programs.fzf.enable = true;
+    fzf.enable = true;
 
-  programs.nh.enable = true;
-  programs.nh.clean.enable = true;
-  programs.nh.flake = "${config.home.homeDirectory}/${namespace}";
-  home.shellAliases.nhs = "nh home switch";
-  home.shellAliases.nos = "nh os switch";
+    nh = {
+      enable = true;
+      clean.enable = true;
+      flake = "${config.home.homeDirectory}/${namespace}";
+    };
 
-  programs.nix-index.enable = true;
+    nix-index.enable = true;
+  };
+
+  home.shellAliases = {
+    nhs = "nh home switch";
+    nos = "nh os switch";
+  };
 
   home.stateVersion = "25.05";
 }
--- nix/homes/x86_64-linux/michael@konya/git.nix
+++ nix/homes/x86_64-linux/michael@konya/git.nix
@@ -1,57 +1,64 @@
 {...}: {
-  programs.git = {
-    enable = true;
-    ignores = [
-      "*~"
-      "*.swp"
-      "*.log"
-      ".direnv"
-      "Session.vim*"
-      ".DS_Store"
-      ".idea/"
-      ".dir-locals.el"
-      "result"
-      ".mise.local.toml"
-      "mise.local.toml"
-      "nohup.out"
-    ];
-    userName = "Michael Utz";
-    userEmail = "michael@theutz.com";
-    extraConfig = {
-      push.autoSetupRemote = "true";
-      pull.rebase = "true";
-      init.defaultBranch = "main";
+  programs = {
+    git = {
+      enable = true;
+      ignores = [
+        "*~"
+        "*.swp"
+        "*.log"
+        ".direnv"
+        "Session.vim*"
+        ".DS_Store"
+        ".idea/"
+        ".dir-locals.el"
+        "result"
+        ".mise.local.toml"
+        "mise.local.toml"
+        "nohup.out"
+      ];
+      userName = "Michael Utz";
+      userEmail = "michael@theutz.com";
+      extraConfig = {
+        push.autoSetupRemote = "true";
+        pull.rebase = "true";
+        init.defaultBranch = "main";
+      };
+
+      delta.enable = true;
+    };
+
+    gh.enable = true;
+
+    lazygit = {
+      enable = true;
+
+      settings = {
+        disableStartupPopups = true;
+        git = {
+          commit = {
+            signoff = true;
+          };
+          paging = {
+            colorArg = "always";
+            parseEmoji = true;
+            overrideGpg = true;
+          };
+        };
+        gui = {
+          border = "rounded";
+          expandFocusedSidePanel = true;
+          nerdFontsVersion = "3";
+          showBottomLine = false;
+          showCommandLog = true;
+          showRandomTip = false;
+        };
+        notARepository = "skip";
+        promptToReturnFromSubprocess = false;
+      };
     };
   };
-  programs.gh.enable = true;
-  programs.git.delta.enable = true;
 
-  programs.lazygit = { 
-    enable = true;
-    settings = {
-      disableStartupPopups = true;
-      git = {
-        commit = {
-          signoff = true;
-        };
-        paging = {
-          colorArg = "always";
-          parseEmoji = true;
-          overrideGpg = true;
-        };
-      };
-      gui = {
-        border = "rounded";
-        expandFocusedSidePanel = true;
-        nerdFontsVersion = "3";
-        showBottomLine = false;
-        showCommandLog = true;
-        showRandomTip = false;
-      };
-      notARepository = "skip";
-      promptToReturnFromSubprocess = false;
-    };
+  home.shellAliases = {
+    lg = "lazygit";
   };
-  home.shellAliases.lg = "lazygit";
-
 }
