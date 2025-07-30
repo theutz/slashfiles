0e0e0c3b57aaf9619217edd42ad2a5e26fbfbffc
Michael Utz <michael@theutz.com>
2025-07-30 16:37:15 +0300

    feat(nvf): mini-pairs

--- nix/packages/nvf/modules/completion.nix
+++ nix/packages/nvf/modules/completion.nix
@@ -1,7 +1,8 @@
 {
-  config.vim.autocomplete = {
-    blink-cmp = {
+  config.vim = {
+    autocomplete.blink-cmp = {
       enable = true;
+
       mappings = {
         close = "<C-e>";
         complete = "<C-space>";
@@ -11,8 +12,24 @@
         scrollDocsDown = "<C-d>";
         scrollDocsUp = "<C-u>";
       };
+
       setupOpts = {
         signature.enabled = true;
+      };
+    };
+
+    mini.pairs = {
+      enable = true;
+      setupOpts = {
+        modes = {
+          insert = true;
+          command = true;
+          terminal = false;
+        };
+        skip_next = ''[[%w%%%'%[%"%.%`%$]]'';
+        skip_ts = ["string"];
+        skip_unbalanced = true;
+        markdown = true;
       };
     };
   };
