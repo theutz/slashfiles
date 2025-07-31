5b8103d7811b6e0f7feb619bc18e9c7b872ff9f9
Michael Utz <michael@theutz.com>
2025-07-31 17:12:43 +0300

    feat: turn on ssh

--- nix/systems/x86_64-linux/konya/default.nix
+++ nix/systems/x86_64-linux/konya/default.nix
@@ -49,7 +49,7 @@
   };
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
