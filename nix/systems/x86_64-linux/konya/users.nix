ad4855cd0602ae4218326cc6719c0d09d3026117
Michael Utz <michael@theutz.com>
2025-08-04 17:42:01 +0300

    chore: _actual_ standlone home-manager mode

--- nix/systems/x86_64-linux/konya/users.nix
+++ nix/systems/x86_64-linux/konya/users.nix
@@ -1,24 +1,24 @@
 {
   pkgs,
   lib,
-  config,
   ...
 }: {
-  users = {
-    users.michael = {
-      isNormalUser = true;
-      createHome = true;
-      group = "michael";
-      extraGroups = ["networkmanager" "wheel"];
-      # packages = with pkgs; [
-      #   sops
-      #   ssh-to-age
-      # ];
-      shell = pkgs.zsh;
-    };
-
-    groups.michael = {};
+  users.users.michael = {
+    isNormalUser = true;
+    createHome = true;
+    group = "michael";
+    extraGroups = ["networkmanager" "wheel"];
+    # packages = with pkgs; [
+    #   sops
+    #   ssh-to-age
+    # ];
+    shell = pkgs.zsh;
   };
+  users.groups.michael = {};
+
+  # Explicitly don't integrate Home Manager, so that
+  # we can manage as standalone with `nh home switch`
+  snowfallorg.users.michael.home.enable = false;
 
   programs.zsh.enable = lib.mkForce true; # required to set default shell to zsh
 }
