366ab94e16fd97f5d0fcfdecbacca97ef2da84ff
Michael Utz <michael@theutz.com>
2025-08-04 21:40:59 +0300

    chore: setup home-manager on konya

--- nix/systems/x86_64-linux/konya/users.nix
+++ nix/systems/x86_64-linux/konya/users.nix
@@ -19,6 +19,9 @@
   # Explicitly don't integrate Home Manager, so that
   # we can manage as standalone with `nh home switch`
   snowfallorg.users.michael.home.enable = false;
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+  home-manager.backupFileExtension = "bak";
 
   programs.zsh.enable = lib.mkForce true; # required to set default shell to zsh
 }
