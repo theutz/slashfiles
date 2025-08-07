8065d8a72099f64cdc0755c39d8f046b0ad3299d
Michael Utz <michael@theutz.com>
2025-08-07 16:50:33 +0300

    chore: bookmarks

--- modules/home/firefox/bookmarks/work.nix
+++ modules/home/firefox/bookmarks/work.nix
@@ -7,10 +7,67 @@
       {
         name = "Mail";
         url = "https://mail.google.com";
+        tags = [
+          "productivity"
+          "communication"
+        ];
       }
       {
         name = "Calendar";
         url = "https://calendar.google.com";
+        tags = [ "productivity" ];
+      }
+      {
+        name = "Projects";
+        url = "https://projects.zoho.com/portal/delegator";
+        tags = [ "productivity" ];
+      }
+      {
+        name = "Slack";
+        url = "https://delegator.slack.com";
+        tags = [ "communication" ];
+      }
+      {
+        name = "OhDear";
+        url = "https://ohdear.app/sites";
+        tags = [
+          "monitoring"
+          "infra"
+        ];
+      }
+      {
+        name = "NetData";
+        url = "https://app.netdata.cloud/spaces/delegator";
+        tags = [
+          "monitoring"
+          "infra"
+        ];
+      }
+      {
+        name = "Linode";
+        url = "https://cloud.linode.com";
+        tags = [ "infra" ];
+      }
+      {
+        name = "Cloudflare";
+        url = "https://dash.cloudflare.com";
+        tags = [ "infra" ];
+      }
+      {
+        name = "HubSpot";
+        url = "https://app.hubspot.com";
+        tags = [
+          "infra"
+          "dev"
+        ];
+      }
+      {
+        name = "GitHub";
+        url = "https://github.com/delegator";
+        tags = [
+          "infra"
+          "dev"
+        ];
       }
     ];
   }
