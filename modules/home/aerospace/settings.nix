179fa6e6a88d648b0a4b30e03c8d79a26d0cb0ef
Michael Utz <michael@theutz.com>
2025-08-08 15:54:43 +0300

    feat(home,aerospace): more shortcuts

--- modules/home/aerospace/settings.nix
+++ modules/home/aerospace/settings.nix
@@ -1,6 +1,29 @@
-{ lib, ... }:
+{ lib, config, ... }:
 let
   gap = 32;
+  bindPerWorkspace =
+    with lib;
+    key: action:
+    let
+      format = template: replacement: replaceString "%d" (toString replacement) template;
+      formatWorkspace = num: nameValuePair (format key num) (format action num);
+    in
+    range 1 9 |> map (formatWorkspace) |> listToAttrs;
+
+  bindPerDirection =
+    with lib;
+    key: action:
+    let
+      dirs = {
+        h = "left";
+        j = "down";
+        k = "up";
+        l = "right";
+      };
+      format = template: replacement: replaceString "%s" replacement template;
+      formatDirs = n: v: nameValuePair (format key n) (format action v);
+    in
+    mapAttrs' (formatDirs) dirs |> listToAttrs;
 in
 {
   after-startup-command = [ ];
@@ -39,10 +62,14 @@ in
     ]
     |> lib.flip lib.elemAt 0;
 
-  gaps = {
-    outer = lib.genAttrs [ "left" "right" "top" "bottom" ] (_: gap);
-    inner = lib.genAttrs [ "horizontal" "vertical" ] (_: gap);
-  };
+  gaps =
+    let
+      gap' = lib.trivial.const gap;
+    in
+    {
+      outer = lib.genAttrs [ "left" "right" "top" "bottom" ] gap';
+      inner = lib.genAttrs [ "horizontal" "vertical" ] gap';
+    };
 
   # All possible keys:
   # - Letters.        a, b, c, ..., z
@@ -75,28 +102,36 @@ in
     {
       slash = "layout tiles horizontal vertical";
       comma = "layout accordion horizontal vertical";
+      b = "exec-and-forget ${config.home.shellAliases.ff}";
+      shift-b = "exec-and-forget ${config.home.shellAliases.ffw}";
       h = "focus --boundaries all-monitors-outer-frame left";
       j = "focus --boundaries all-monitors-outer-frame down";
       k = "focus --boundaries all-monitors-outer-frame up";
       l = "focus --boundaries all-monitors-outer-frame right";
-      "shift-h" = "move --boundaries all-monitors-outer-frame left";
-      "shift-j" = "move --boundaries all-monitors-outer-frame down";
-      "shift-k" = "move --boundaries all-monitors-outer-frame up";
-      "shift-l" = "move --boundaries all-monitors-outer-frame right";
+      shift-h = "move --boundaries all-monitors-outer-frame left";
+      shift-j = "move --boundaries all-monitors-outer-frame down";
+      shift-k = "move --boundaries all-monitors-outer-frame up";
+      shift-l = "move --boundaries all-monitors-outer-frame right";
+      n = "workspace --wrap-around next";
+      shift-n = "move-node-to-workspace next";
+      p = "workspace --wrap-around prev";
+      shift-p = "move-node-to-workspace prev";
       minus = "resize smart -50";
       equal = "resize smart +50";
+      f13 = "mode power";
+      m =
+        ''
+          exec-and-forget ${lib.getExe config.programs.wezterm.package} start --always-new-process
+        ''
+        |> lib.trim;
     }
-    // (
-      lib.range 1 9
-      |> lib.map builtins.toString
-      |> lib.map (x: lib.nameValuePair x "workspace ${x}")
-      |> lib.listToAttrs
-    )
-    // (
-      lib.range 1 9
-      |> lib.map builtins.toString
-      |> lib.map (x: lib.nameValuePair "shift-${x}" "move-node-to-workspace ${x}")
-      |> lib.listToAttrs
-    )
+    // (bindPerWorkspace "%d" "workspace %d")
+    // (bindPerWorkspace "shift-%d" "move-node-to-workspace %d")
   );
+
+  mode.power.binding = {
+    esc = "mode main";
+    f13 = "mode main";
+  }
+  // (bindPerWorkspace "%d" "workspace %d");
 }
